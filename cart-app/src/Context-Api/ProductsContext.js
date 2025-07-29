ddc21b83f7349634ec2eaf9132284a91445c21bb
surya2prakash <sv251171@gmail.com>
2025-07-29 12:46:56 +0530

    Context api is created

---
+++ cart-app/src/Context-Api/ProductsContext.js
@@ -0,0 +1,81 @@
+
+import { createContext, useEffect, useState } from "react";
+
+
+export const productContext = createContext();
+
+
+let baseUrl = "https://dummyjson.com/carts "
+
+export default function CartConextProvider({children}){
+   
+      const [products,setProducts] = useState([]);
+      const [cartItems,setCartitems]=useState([]);
+      
+
+      useEffect(()=>{
+
+        async function fetchProductData(){
+          try{
+
+            const response = await fetch(baseUrl,{
+                method:"GET",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            });
+
+            const result = await response.json();
+
+                    
+            
+
+            setProducts(result.carts?.[1]?.products || []);
+
+
+          }catch(err){
+            console.error(err);
+
+          }
+        }
+
+        fetchProductData();
+
+      },[]);
+
+
+      
+
+
+      //add krne pe cartItem me kya show hoga 
+      const addToCart =(product)=>{
+        //prev item ko to rakhna hi hai aur jo add hua hai usko bhi add kr lena hai
+     
+           setCartitems((prev)=>[...prev,product])
+      }
+
+      //remove krne pe cartItem me kya show hoga
+
+
+      const removeformCart = (id)=>{
+           setCartitems( prev => prev.filter(item =>item.id !==id))
+      }
+      //total price ?
+      
+      const totalPrice = cartItems.reduce((acc,item) => acc +item.price,0) ;
+      
+      let value ={products,
+        addToCart,
+        removeformCart,
+        totalPrice,
+        cartItems,
+        
+      }
+
+      return(
+          <productContext.Provider value={value}>
+            {children}
+          </productContext.Provider>
+      )
+
+}
