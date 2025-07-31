45abb263eac4fbbd341e4b38c8e65b903681261d
surya2prakash <sv251171@gmail.com>
2025-07-31 08:58:37 +0530

    cartPage logic is added

--- cart-app/src/Component/CartPage.js
+++ cart-app/src/Component/CartPage.js
@@ -1,29 +1,30 @@
 import React, { useContext } from 'react'
 import { productContext } from '../Context-Api/ProductsContext'
 
-
+import "./CartPage.css"
 export default function CartPage() {
 
     const cartItems = useContext(productContext);
 
    
     const {removeformCart,totalPrice} = useContext(productContext);
+    let amount = Math.floor(totalPrice);
   return (
-    <div>
+    <div className='cart_page_container'>
         
         {
             
            cartItems?.cartItems.length > 0 ?
       (
-        <div>
+        <div className='cart_page_main'>
             {
            cartItems?.cartItems.map((item)=>(
                
-                <div key={item.id}>
+                <div key={item.id} className='cart_page'>
                      
-                <div>Item Name : ${item.title}</div>
-                <div>Price : ${item.price}</div>
-                <div>
+                <div className='item_name'>Item Name : {item.title}</div>
+                <div className='item_Price'>Price : ${item.price}</div>
+                <div className='item_img'>
                     <img src={item.thumbnail} alt='not found'/>
                 </div>
                 <button onClick={()=>{removeformCart(item.id)}}>Remove Item</button>
@@ -32,11 +33,11 @@ export default function CartPage() {
                 
             ))
         }
-            <div>
-             <div>Total Price : ${totalPrice}</div>
+            <div className='price_conainer'>
+             <div className='total_price'>Total Price : ${amount}</div>
              </div>
              </div>)
-            :(<div>No Item</div>)
+            :(<div className='empty_items'>No Item</div>)
         }
     </div>
   )
